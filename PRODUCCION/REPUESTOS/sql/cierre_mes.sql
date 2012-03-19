5889c24652065ebb5da3287343d77510b286b64f
Jaime Casanova <jcasanov@systemguards.com.ec>
2012-03-19 12:24:33 -0500

    Agregar el script de cierre de mes a los fuentes

--- PRODUCCION/REPUESTOS/sql/cierre_mes.sql
+++ PRODUCCION/REPUESTOS/sql/cierre_mes.sql
@@ -1,11 +1,20 @@
+BEGIN WORK;
 
 INSERT INTO rept031
-		SELECT r11_compania, 2009, 9
-		, r11_bodega, r11_item, r11_stock_act,
-		r10_costo_mb, r10_costo_ma, r10_precio_mb,
-		r10_precio_ma
-		 FROM rept011_sept30, rept010_sept30
-		WHERE r11_compania  = 1
-		  AND r11_stock_act > 0
-		  AND r10_compania  = r11_compania
-		  AND r10_codigo    = r11_item;
+SELECT r11_compania,
+       (SELECT YEAR(current - 10 UNITS DAY) FROM dual),
+       (SELECT MONTH(current - 10 UNITS DAY) FROM dual),
+       r11_bodega, r11_item, r11_stock_act,
+       r10_costo_mb, r10_costo_ma, r10_precio_mb,
+       r10_precio_ma
+  FROM rept011, rept010
+ WHERE r11_compania  = 1
+   AND r11_stock_act > 0
+   AND r10_compania  = r11_compania
+   AND r10_codigo    = r11_item;
+
+UPDATE rept000 SET r00_anopro = (SELECT YEAR(current) FROM dual),
+                   r00_mespro = (SELECT MONTH(current) FROM dual)
+ WHERE r00_compania = 1;
+
+COMMIT WORK;
