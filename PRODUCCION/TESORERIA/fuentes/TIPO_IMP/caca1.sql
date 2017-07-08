6cfa58264bfed9e102d286fe45d957156eb6ea76
Jaime Casanova <jaime.casanova@2ndquadrant.com>
2017-07-07 21:36:57 -0500

    Eliminando archivos basura

--- PRODUCCION/TESORERIA/fuentes/TIPO_IMP/caca1.sql
+++
@@ -1,8 +0,0 @@
-begin work;
-DELETE FROM ordt003
-                        WHERE c03_compania       = 1
-                          AND c03_tipo_ret       = 'F'
-                          AND c03_porcentaje     = 1
-                          AND c03_codigo_sri     = '307'
-                          AND c03_fecha_ini_porc = mdy(04,01,2003);
-rollback work;
