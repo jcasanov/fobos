9ef33c7c5c0a419ac2d5a7672d2f786d4fa12606
Jaime Casanova <jaime.casanova@2ndquadrant.com>
2017-08-07 19:31:09 -0500

    Arreglar la sintaxis de los ALTER TABLE
    Parche: JCM

--- PRODUCCION/GENERALES/sql/fobos_alt.sql
+++ PRODUCCION/GENERALES/sql/fobos_alt.sql
@@ -1,5 +1,10 @@
-alter table fobos add fb_usar_fechasist char(1) check ('S', 'N');
+begin work;
+
+alter table fobos add fb_usar_fechasist char(1);
+alter table fobos add constraint check (fb_usar_fechasist in ('S', 'N'));
 update fobos set fb_usar_fechasist = 'N'; 
 alter table fobos modify fb_usar_fechasist char(1) not null;
 
 alter table fobos add fb_fechasist date;
+
+commit work;
